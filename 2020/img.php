8b5314c041039501a165ce270308a7340f171027
U1805 <13868750576@163.com>
2020-01-18 14:22:55 +0800

    Site updated: 2020-01-18 14:22:55

--- 2020/img.php
+++
@@ -1,8 +0,0 @@
-<?php
- //这将得到一个文件夹中的所有gif，jpg和png图片的数组 
-$img_array = glob("./images/*/*.{gif,jpg,png}",GLOB_BRACE);
- //从数组中选择一个随机图片 
-$img = array_rand($img_array);
-//在页面显示图片 
-header("location:.$img_array[$img]"); 
- ?>
